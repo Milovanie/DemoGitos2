e6390851391fb02596ef8f9c15aec576c10af66d
Sveta77 <rimma34gmail.com>
2018-04-28 13:59:58 +0200

    initial check sales 1

---
+++ DemoGit1/src/hello/He.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class He {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesDate s = new SalesDate();
+		s.display();
+	    System.out.println( "Hello World!" );
+	}
+
+}
---
+++ DemoGit1/src/hello/SalesDate.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class SalesDate {
+	int data[] = {0, 4, 42};
+
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("data display");
+	}
+
+}
